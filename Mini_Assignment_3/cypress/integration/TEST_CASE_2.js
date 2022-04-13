66234afab53f75ea9729a348735fdd62f86649ae
Kumar <anuragkumar5@deloitte.com>
2022-04-13 19:44:10 +0530

    Click ‘Check box’ button

--- Mini_Assignment_3/cypress/integration/TEST_CASE_2.js
+++ Mini_Assignment_3/cypress/integration/TEST_CASE_2.js
@@ -16,5 +16,21 @@ describe('My First Test', () =>
     it('Click Check box button found at the left side, expand the Home option, and select the Documents check box ',function()
     {
         cy.get("div.element-group:nth-child(1)  > div > ul >  #item-1").click()
+        cy.get("#tree-node-home").check({force: true}).should('be.checked')
+    })
+    it('Validating Home check result', () =>
+     {
+        cy.get('#result').should('contain.text', 'home')
+        cy.get('#result').should('contain.text', 'desktop')
+        cy.get('#result').should('contain.text', 'notes')
+        cy.get('#result').should('contain.text', 'commands')
+        cy.get('#result').should('contain.text', 'documents')
+     })
+     it('Validating Home checkbox deselection', () => {
+        cy.get('#tree-node-home').uncheck({force: true}).should('not.be.checked')
+    })
+    it('Validating Documents checkbox selection', () => {
+        cy.get("button[title*='Toggle']").click()
+        cy.get('#tree-node-documents').check({force: true}).should('be.checked')
     })
 })
