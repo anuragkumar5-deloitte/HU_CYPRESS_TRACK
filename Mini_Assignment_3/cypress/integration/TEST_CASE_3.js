3bbb3b05b8b43c7d9d57907996ec83166a05b0e1
Kumar <anuragkumar5@deloitte.com>
2022-04-13 20:22:09 +0530

    Click on ‘Radio Button’

--- Mini_Assignment_3/cypress/integration/TEST_CASE_3.js
+++ Mini_Assignment_3/cypress/integration/TEST_CASE_3.js
@@ -16,5 +16,23 @@ describe('My First Test', () =>
     it('Click on Radio Button found at the left side, select the impressive option. assert that correct option is selected',function()
     {
         cy.get("div.element-group:nth-child(1)  > div > ul >  #item-2").click()
+        cy.get('#app > div > div > div.pattern-backgound.playgound-header > div').should('contain.text', 'Radio Button')
     })
+    it('Validating the url for text-box page', () => {
+        cy.url().should('contain', '/radio-button')
+    })
+
+    it('Validating yes radio button selected in results', () => {
+        cy.get('#yesRadio').check({force: true})
+        cy.get('div:nth-child(2) > p > span').should('contain.text','Yes')
+    })
+
+    it('Validating Impressive radio button selected in results', () => {
+        cy.get('#impressiveRadio').check({force: true})
+        cy.get('div:nth-child(2) > p > span').should('contain.text','Impressive')
+    })
+
+    it('Validating No radio button is disabled', () => {
+        cy.get('#noRadio').should('be.disabled')
 })
+ })
